6ab8541386e0f4460896150e3eb865e4df87e418
Vasily <pisarevvv@gmail.com>
2021-05-19 22:02:24 +0300

    Ensured `dbload` tests use the updated API

--- test/dbload.jl
+++ test/dbload.jl
@@ -9,24 +9,25 @@ end
 
 @testset "src/dbload.jl" begin
     @testset "Default databases" begin
-        comp_explicit = load(BrusilovskyEoSComponent,
+        comp_explicit = load(
+            BrusilovskyEoSComponent;
             name="methane",
-            physics_db=Data.martinez(),
-            eos_db=Data.brusilovsky_comp()
+            component_dbs=(Data.martinez(), Data.brusilovsky_comp())
         )
         comp_default = load(BrusilovskyEoSComponent,
             name="methane"
         )
         compare_structs(comp_explicit, comp_default)
 
-        mix_explicit = load(BrusilovskyEoSMixture,
-               names = ("methane", "n-pentane"),
-               comp_physics_db = Data.martinez(),
-               comp_eos_db = Data.brusilovsky_comp(),
-               mix_eos_db = Data.brusilovsky_mix()
+        mix_explicit = load(
+            BrusilovskyEoSMixture;
+            names = ("methane", "n-pentane"),
+            component_dbs=(Data.martinez(), Data.brusilovsky_comp()),
+            mix_eos_db = Data.brusilovsky_mix()
         )
-        mix_default = load(BrusilovskyEoSMixture,
-               names = ("methane", "n-pentane"),
+        mix_default = load(
+            BrusilovskyEoSMixture;
+            names = ("methane", "n-pentane"),
         )
         compare_structs(mix_explicit, mix_default)
     end

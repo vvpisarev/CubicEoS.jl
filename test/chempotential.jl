9c28d294734498686a33c9dff60bd89fc2f1e429
Степан Захаров <stepanzh@gmail.com>
2021-06-30 14:57:18 +0300

    log_c_activity tests: f(x) vs f!(x) version, buffered call, homogeneity

--- test/chempotential.jl
+++ test/chempotential.jl
@@ -1,3 +1,24 @@
 @testset "chempotential.jl" begin
-    @test 1 == 1
+    C₁C₅ = CubicEoS.load(BrusilovskyEoSMixture; names=("methane", "n-pentane"))
+    N = 1e-2 .+ rand(2)
+    V = 1e-2 + rand()
+    RT = 300 * CubicEoS.GAS_CONSTANT_SI
+
+
+    @testset "func vs func! versions" begin
+        vec = similar(N)
+        log_c_activity!(vec, C₁C₅, N, V, RT)
+        @test vec == log_c_activity(C₁C₅, N, V, RT)
+    end
+
+    @testset "Buffered versions" begin
+        buf = thermo_buffer(C₁C₅)
+        @test log_c_activity(C₁C₅, N, V, RT) == log_c_activity(C₁C₅, N, V, RT; buf = buf)
+    end
+
+    @testset "Homogeneity" begin
+        for α in 0.1:0.2:1.9
+            @test log_c_activity(C₁C₅, N, V, RT) ≈ log_c_activity(C₁C₅, α .* N, α * V, RT)
+        end
+    end
 end

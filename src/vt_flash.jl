7d62b0dce53556e9eebdbee0b244a286d9b22850
Степан Захаров <stepanzh@gmail.com>
2021-11-26 14:09:32 +0300

    [vtflash] decompose algo into stages
    For now, general vt_flash is structured as follows
    
    1. Perform vt_stability and check for single state
    2. (new) Find initial state using (separate procedure)
    3. (new) Run vt_flash procedure with from initial state (separate procedure)

--- src/vt_flash.jl
+++ src/vt_flash.jl
@@ -330,6 +330,35 @@ function vt_flash_closures(
     return constrain_step, helmholtz_diff_grad!, helmholtz_diff!
 end
 
+"Find initial state by reducing saturation."
+function __vt_flash_initial_state(
+    mix::BrusilovskyEoSMixture,
+    nmol::AbstractVector,
+    volume::Real,
+    RT::Real,
+    stability_tries::AbstractVector{VTStabilityResult{T}};
+    sat₁max::Real=0.25,
+    steps::Integer=200,
+    step_scale::Real=0.5,
+    helmholtz_thresh::Real=-1e-7,
+) where {T}
+    constrain_step, _, helmholtz_diff! = vt_flash_closures(mix, nmol, volume, RT)
+
+    state = Vector{T}(undef, ncomponents(mix) + 1)
+
+    # choosing concentration with minimum of helmholtz density
+    conc_test = __vt_flash_init_conc_choose(stability_tries)
+
+    init_found = __vt_flash_initial_state!(
+        state, nmol, volume, conc_test, helmholtz_diff!, constrain_step;
+        sat₁max=sat₁max,
+        steps=steps,
+        step_scale=step_scale,
+        helmholtz_thresh=helmholtz_thresh,
+    )
+    return init_found, state
+end
+
 function __vt_flash_initial_state!(
     state::AbstractVector{T},
     nmol::AbstractVector{T},
@@ -409,7 +438,7 @@ function __vt_flash_two_phase_result(
         end
     end
 
-    return VTFlashResult{T}(
+    return VTFlashResult{T}(;
             converged=optresult.converged,
             singlephase=false,
             RT=RT,
@@ -422,6 +451,36 @@ function __vt_flash_two_phase_result(
     )
 end
 
+"Perform vt_flash from `unstable_state`, so only phase splitting is done."
+function vt_flash(
+    mix::BrusilovskyEoSMixture{T},
+    nmol::AbstractVector,
+    volume::Real,
+    RT::Real,
+    unstable_state::AbstractVector,
+) where {T}
+    state = copy(unstable_state)
+
+    # initial hessian
+    hessian = Matrix{T}(undef, (size(state, 1), size(state, 1)))
+    __vt_flash_hessian!(hessian, state, mix, nmol, volume, RT)
+
+    # create closures for helmoltz energy, its gradient and constrain step
+    constrain_step, _, helmholtz_diff! = vt_flash_closures(mix, nmol, volume, RT)
+
+    # run optimizer
+    optmethod = DescentMethods.CholBFGS(state)
+    DescentMethods.reset!(optmethod, state, hessian)
+    result = DescentMethods.optimize!(optmethod, helmholtz_diff!, state;
+        gtol=1e-3,
+        maxiter=100,
+        constrain_step=constrain_step,
+        reset=false,
+    )
+    return __vt_flash_two_phase_result(mix, nmol, volume, RT, result)
+end
+
+"VT-flash."
 function vt_flash(
     mix::BrusilovskyEoSMixture{T},
     nmol::AbstractVector,
@@ -430,63 +489,36 @@ function vt_flash(
 ) where {T}
     # run vt-stability to find out whether a state single phase or not
     singlephase, vt_stab_tries = vt_stability(mix, nmol, volume, RT)
+
     @debug "VTFlash: VTStability result" singlephase
 
     if singlephase
-        return VTFlashResult{T}(
+        return VTFlashResult{T}(;
             converged=true,
             singlephase=true,
             RT=RT,
             nmol_1=nmol,
             V_1=volume,
             nmol_2=similar(nmol),
-            V_2=0
+            V_2=0,
         )
     end
 
     # two-phase state case
-    # create closures for helmoltz energy, its gradient and constrain step
-    constrain_step, helmholtz_diff_grad!, helmholtz_diff! = vt_flash_closures(
-        mix, nmol, volume, RT
-    )
-
-    # find initial vector for optimizer
-    state = Vector{T}(undef, ncomponents(mix) + 1)
-    η₁test = __vt_flash_init_conc_choose(vt_stab_tries)
-
-    init_found = __vt_flash_initial_state!(
-        state, nmol, volume, η₁test, helmholtz_diff!, constrain_step;
+    init_found, state = __vt_flash_initial_state(
+        mix, nmol, volume, RT, vt_stab_tries;
         sat₁max=0.25,
         steps=200,
         step_scale=0.5,
         helmholtz_thresh=-1e-7,
     )
 
-    @debug "VTFlash: initial state search result" found=init_found state=repr(state) ΔA=helmholtz_diff!(state, similar(state))
+    @debug "VTFlash: initial state search result" found=init_found state=repr(state) ΔA=helmholtz_diff!(state, similar(state))[1]
 
     if !init_found
         @error "VTFlash: Initial state was not found!" mixture=mix nmol=repr(nmol) volume=volume RT=RT
         error("VTFlash: Initial state was not found!")
     end
 
-    # initial hessian
-    hessian = Matrix{T}(undef, (length(state), length(state)))
-    __vt_flash_hessian!(hessian, state, mix, nmol, volume, RT)
-    @debug "VTFlash: initial hessian found" isposdef(hessian)
-
-    # run optimizer
-    optmethod = DescentMethods.CholBFGS(state)
-    DescentMethods.reset!(optmethod, state, hessian)
-    result = DescentMethods.optimize!(
-        optmethod,
-        helmholtz_diff!,
-        state,
-        gtol=1e-3,
-        maxiter=100,
-        constrain_step=constrain_step,
-        reset=false,
-    )
-    # TODO: check convergence of BFGS
-
-    return __vt_flash_two_phase_result(mix, nmol, volume, RT, result)
+    return vt_flash(mix, nmol, volume, RT, state)
 end

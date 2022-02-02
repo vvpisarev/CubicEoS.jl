4dfe4a81d0dc14efc4bf0fbb46eeb704eb51528f
Степан Захаров <stepanzh@gmail.com>
2022-02-02 20:33:32 +0300

    [test vtflash] add comparing of vtflash for 3 possible internal variables

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,7 +19,7 @@ end
     include("chempotential.jl")
     include("solvecubic.jl")
     include("newton.jl")
-    include("vt_flash/vt_flash.jl")
     include("vt_flash/nvt.jl")
     include("vt_flash/state_idealidentity.jl")
+    include("vt_flash/vt_flash.jl")
 end
